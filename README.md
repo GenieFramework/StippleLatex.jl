4138c1aa0e09f1b849390e201217f8fae7b9967b
Helmut Hänsel <helmut.haensel@gmx.com>
2023-02-22 11:04:05 +0100

    update readme

--- README.md
+++ README.md
@@ -24,6 +24,7 @@ using Stipple, Stipple.ReactiveTools
 using StippleUI
 using StippleLatex
 
+# define a small formula generator
 function nestlist(f, a; init = nothing)
     T = eltype(a)
     list = T[]
@@ -38,6 +39,7 @@ end
 formula = nestlist(*, ["", raw"\sin", "^2", " x", " +", raw" \sqrt{", "a", "^2", " +", " b", "^2"])
 formula[contains.(formula, "sqrt")] .*= "}"
 
+# setting up the app
 @app begin
     @in x = 0
     @in formula_1 = raw"\int_{a}^{b} f(x) \, dx = F(x)\Biggr|^b_a"
@@ -49,10 +51,10 @@ formula[contains.(formula, "sqrt")] .*= "}"
             p = @task begin
                 println("Task started")
                 while x <= 100
-                    x = x + 1
+                    sleep(1)
+                    x += 1
                     pos = x < 6 ? 1 : (x - 5) % (length(formula) + 5) + 1
                     formula_2 = formula[min(pos, length(formula))]
-                    sleep(1)
                 end
             end
             schedule(p)
@@ -60,7 +62,6 @@ formula[contains.(formula, "sqrt")] .*= "}"
     end
 end
 
-@init()
 function ui()
     [
         row(cell(class = "st-module", [
@@ -70,18 +71,37 @@ function ui()
 
         row(cell(class = "st-module", [
             textfield("Enter your LaTeX-Forumla", :formula_1,)
-            cell(class = "q-pa-md", latex":formula_1")
-            cell(class = "q-pa-md", latex"\cos^2x"display)
-            latex(class = "q-pa-md", raw"\tan^2x", display = true)
+            cell(class = "q-pa-md", latex":formula_1"display)
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""cell(latex"\cos^2x"display)""")
+                cell(class = "q-pa-md bg-green-1", latex"\cos^2x"display)
+            ])
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""cell(latex"This is auto mode with a formula \(\cos^2x\)"auto)""")
+                cell(class = "q-pa-md bg-green-1", latex"This is auto mode with a formula \(\cos^2x\)"auto)
+            ])
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""latex(class = "q-pa-md", raw"\tan^2x", display = true)""")
+                cell(class = "bg-green-1 q-pa-md", latex(class = "q-pa-md", raw"\tan^2x", display = true))
+            ])
             bignumber("Wait for 5", :x, color = R"x >= 5 ? 'negative' : 'positive'", icon = "calculate")
-            cell(class = "q-pa-md", @latex(raw"\tanh^2 y", display = R"x >= 5"))
+            
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""cell(class = "q-pa-md", @latex(raw"\tanh^2 y", display = R"x >= 5"))""")
+                cell(class = "q-pa-md bg-green-1", @latex(raw"\tanh^2 y", display = R"x >= 5"))
+            ])
+
+
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""cell(class = "q-pa-md", @latex(raw"\tanh^2 y", display = R"x >= 5"))""")
+                cell(class = "q-pa-md bg-green-1", @latex(raw"This is auto mode with an inline formula \(\cos^2x\) and a display formula $$\sin^2x$$", auto = true))
+            ])
         ]))
 
         row(cell(class = "st-module", [
             textfield(class = "q-pa-lg", "LaTeX", :formula_2)
             cell(class = "q-pa-md", "Result:")
-            cell(class = "q-pa-md", latex":formula_2"auto)
-
+            cell(class = "q-pa-md", latex":formula_2"display)
         ]))
     ]
 end
@@ -93,3 +113,4 @@ end
 up()
 ```
 
+![Demo App](./docs/demo.png)
--- docs/example1.jl
+++ docs/example1.jl
@@ -1,35 +1,91 @@
-using Genie, Genie.Renderer.Html, Stipple, StippleLatex, LatexPrint
+using Stipple, Stipple.ReactiveTools
+using StippleUI
+using StippleLatex
 
-Genie.config.log_requests = false
-# Genie.config.base_path = "/proxy/9000/"
-
-Base.@kwdef mutable struct Name <: ReactiveModel
-  name::R{String} = "World!"
+# define a small formula generator
+function nestlist(f, a; init = nothing)
+    T = eltype(a)
+    list = T[]
+    el = init
+    for (i, x) in enumerate(a)
+        el = i == 1 && init === nothing ? x : f(el, x)
+        push!(list, el)
+    end
+    list
 end
 
-Stipple.register_components(Name, StippleLatex.COMPONENTS)
+formula = nestlist(*, ["", raw"\sin", "^2", " x", " +", raw" \sqrt{", "a", "^2", " +", " b", "^2"])
+formula[contains.(formula, "sqrt")] .*= "}"
 
-model = Stipple.init(Name(), transport = Genie.WebChannels)
+# setting up the app
+@app begin
+    @in x = 0
+    @in formula_1 = raw"\int_{a}^{b} f(x) \, dx = F(x)\Biggr|^b_a"
+    @in formula_2 = raw""
+    @private p = @task 1 + 1
+
+    @onchange isready begin
+        if !istaskstarted(p) || istaskdone(p) 
+            p = @task begin
+                println("Task started")
+                while x <= 100
+                    sleep(1)
+                    x += 1
+                    pos = x < 6 ? 1 : (x - 5) % (length(formula) + 5) + 1
+                    formula_2 = formula[min(pos, length(formula))]
+                end
+            end
+            schedule(p)
+        end
+    end
+end
 
 function ui()
-  page(
-    vm(model), class="container",
     [
-      h1([
-        "Hello "
-        span("", @text(:name), @latex!!(:name))
-      ])
+        row(cell(class = "st-module", [
+            cell(h1(latex("\\LaTeX") * "-Demo"))
+            cell(h2(latex"a^2 + b^2 = c^2"))
+        ]))
 
-      p([
-        "What is your name? "
-        input("", placeholder="Type your name", @bind(:name))
-      ])
+        row(cell(class = "st-module", [
+            textfield("Enter your LaTeX-Forumla", :formula_1,)
+            cell(class = "q-pa-md", latex":formula_1"display)
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""cell(latex"\cos^2x"display)""")
+                cell(class = "q-pa-md bg-green-1", latex"\cos^2x"display)
+            ])
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""cell(latex"This is auto mode with a formula \(\cos^2x\)"auto)""")
+                cell(class = "q-pa-md bg-green-1", latex"This is auto mode with a formula \(\cos^2x\)"auto)
+            ])
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""latex(class = "q-pa-md", raw"\tan^2x", display = true)""")
+                cell(class = "bg-green-1 q-pa-md", latex(class = "q-pa-md", raw"\tan^2x", display = true))
+            ])
+            bignumber("Wait for 5", :x, color = R"x >= 5 ? 'negative' : 'positive'", icon = "calculate")
+            
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""cell(class = "q-pa-md", @latex(raw"\tanh^2 y", display = R"x >= 5"))""")
+                cell(class = "q-pa-md bg-green-1", @latex(raw"\tanh^2 y", display = R"x >= 5"))
+            ])
 
-      # latex(raw"\\frac{a_i}{1+x}", display = true)
+
+            row([
+                cell(class = "q-pa-md bg-red-1", raw"""cell(class = "q-pa-md", @latex(raw"\tanh^2 y", display = R"x >= 5"))""")
+                cell(class = "q-pa-md bg-green-1", @latex(raw"This is auto mode with an inline formula \(\cos^2x\) and a display formula $$\sin^2x$$", auto = true))
+            ])
+        ]))
+
+        row(cell(class = "st-module", [
+            textfield(class = "q-pa-lg", "LaTeX", :formula_2)
+            cell(class = "q-pa-md", "Result:")
+            cell(class = "q-pa-md", latex":formula_2"display)
+        ]))
     ]
-  ) |> html
 end
 
-route("/", ui)
-
+route("/") do
+    page(@init(), ui()) |> html
+end
+    
 up()
